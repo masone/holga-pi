b5d5702a6171c8a6880078be3eeea49efd0cee77
Christian Felder (masone) <ema@rh-productions.ch>
2017-10-25 13:26:31 +0200

    snapping

--- camera/index.js
+++ camera/index.js
@@ -6,36 +6,22 @@ const _ = require('lodash');
 const output = path.resolve('./tmp/holgapi%d.jpg');
 const camera = new RaspiCam({mode: 'photo', output});
 
-button = new Gpio(4, 'in', 'rising', {activeLow: true}) //, {activeLow: false});
+button = new Gpio(4, 'in', 'rising', {activeLow: true}); //, {activeLow: false});
 const trigger = _.debounce(() => {
   console.log('throttled')
 }, 1000);
 
-console.log('listening...')
-button.watch(function(err, value) {
+console.log('listening...');
+button.watch(function (err, value) {
   if (err) console.log(err);
   trigger();
 });
 
-// gpio.open(pin, 'input pulldown', function(err) {
-//   if(err) throw err;
-//   console.log('gpio ready');
-//
-//   setInterval(() => {
-//     gpio.read(pin, function(err, value) {
-//       if(err) console.log("gpio error", err);
-//       console.log('read value', value)
-//     });
-//   }, 0);
-// });
-
-
-// camera.start();
-// camera.on("read", (err, timestamp, filename) => {
-//   if(err) console.log("Error", err);
-//
-//   console.log('stuff', timestamp, filename);
-// });
-//
-// RaspiCam.stop();
-//
+const snap = () => {
+  camera.start();
+  camera.on('read', (err, timestamp, filename) => {
+    if (err) console.log('Error', err);
+    console.log('snapped', filename);
+    RaspiCam.stop();
+  });
+};
