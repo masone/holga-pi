8a7ee01b4e457c0d2681ce9b7fe260e4f2b60f4b
Christian Felder <ema@rh-productions.ch>
2017-10-25 16:58:14 +0000

    fix it

--- camera/index.js
+++ camera/index.js
@@ -4,12 +4,39 @@ const path = require('path');
 const cloudinary = require('cloudinary');
 const _ = require('lodash');
 
-const output = path.resolve(`./tmp/holga.jpg`);
+const output = path.resolve('./tmp/holga.jpg');
+const camera = new RaspiCam({
+  rotation: 270, contrast: 0, saturation: 0,
+  nopreview: true, vstab: false,
+	timeout: 0, //framerate: 1,
+  mode: 'timelapse', output,
+	timelapse: 1
+	//pi@raspberrypi:~/holga-pi/camera $ raspistill --timelapse 1 -o tmp/img_%04d.jpg --latest tmp/img_latest.jpg --timeout 0
+	////width: 1296, height: 972, quality: 80, encoding: 'jpg', output
+});
+
+  camera.on('read', (err, timestamp, filename) => {
+	      if (err) return console.log('Error', err);
+	      console.log('snapped', timestamp, filename);
+  });
+
+  camera.on('stop', () => {
+    console.log('stooop')	    
+  });
+
+  camera.on('exit', () => {
+	      console.log('exiit')
+	    });
+
+
+//setInterval(() => { 
+	camera.start();
+//}, 1000)
 
 const button = new Gpio(4, 'in', 'rising', {activeLow: true});
 const trigger = _.debounce(() => {
   console.log('triggering...')
-  snap();
+  upload();
 }, 1500, {leading: true, trailing: false});
 
 console.log('listening...');
@@ -18,27 +45,10 @@ button.watch(function (err, value) {
   trigger();
 });
 
-const snap = () => {
-  const timestamp = Math.floor(new Date() / 1000);
-  const output = path.resolve(`./tmp/holga-${timestamp}.jpg`);
-  const camera = new RaspiCam({
-	  rotation: 270, contrast: 0, saturation: 0,
-	  nopreview: true, vstab: false,  
-	  mode: 'photo', width: 1296, height: 972, quality: 80, encoding: 'jpg', output
-  });
-  console.log('snapping...');
-  camera.on('read', (err) => {
-    if (err) return console.log('Error', err);
-    console.log('snapped', output);
-    upload(output);
-  });
-  camera.start();
-};
-
-const upload = (file) => {
+const upload = () => {
   console.log('uploading...');
-  cloudinary.uploader.upload(file, function (result) {
+  cloudinary.uploader.upload(output, function (result) {
     console.log(result);
-    console.log('uploaded', file);
+    console.log('uploaded');
   });
 };
