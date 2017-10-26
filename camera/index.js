32865e03ea11a05a39603ea96b4f3c06b323c76f
Christian Felder (masone) <ema@rh-productions.ch>
2017-10-26 09:12:56 +0200

    lint

--- camera/index.js
+++ camera/index.js
@@ -1,44 +1,51 @@
-const RaspiCam = require('raspicam');
-const Gpio = require('onoff').Gpio;
-const path = require('path');
-const cloudinary = require('cloudinary');
-const _ = require('lodash');
+const RaspiCam = require('raspicam')
+const Gpio = require('onoff').Gpio
+const path = require('path')
+const cloudinary = require('cloudinary')
+const _ = require('lodash')
 
-const output = path.resolve(`./tmp/holga.jpg`);
+const output = path.resolve(`./tmp/holga.jpg`)
 const camera = new RaspiCam({
-  rotation: 270, contrast: 30, saturation: 30,
-  mode: 'photo', width: 2592, height: 1944, quality: 100, encoding: 'jpg', output
-});
+  rotation: 270,
+  contrast: 30,
+  saturation: 30,
+  mode: 'photo',
+  width: 2592,
+  height: 1944,
+  quality: 100,
+  encoding: 'jpg',
+  output
+})
 
-const button = new Gpio(4, 'in', 'rising', {activeLow: true});
+const button = new Gpio(4, 'in', 'rising', {activeLow: true})
 const trigger = _.debounce(() => {
-  snap();
-}, 1000);
+  snap()
+}, 1000)
 
-console.log('listening...');
+console.log('listening...')
 button.watch(function (err, value) {
-  if (err) return console.log('Error watching button', err);
-  trigger();
-});
+  if (err) return console.log('Error watching button', err)
+  trigger()
+})
 
 const snap = () => {
-  console.log('snapping...');
-  const timestamp = Math.floor(new Date() / 1000);
-  const output = path.resolve(`./tmp/holga-${timestamp}.jpg`);
-  camera.set('output', output);
-  camera.start();
+  console.log('snapping...')
+  const timestamp = Math.floor(new Date() / 1000)
+  const output = path.resolve(`./tmp/holga-${timestamp}.jpg`)
+  camera.set('output', output)
+  camera.start()
   camera.on('read', (err) => {
-    if (err) return console.log('Error reading from camera', err);
-    console.log('snapped', output);
-    camera.stop();
-    upload(output);
-  });
-};
+    if (err) return console.log('Error reading from camera', err)
+    console.log('snapped', output)
+    camera.stop()
+    upload(output)
+  })
+}
 
 const upload = (file) => {
-  console.log('uploading...');
+  console.log('uploading...')
   cloudinary.uploader.upload(file, function (result) {
-    console.log(result);
-    console.log('uploaded', file);
-  });
-};
+    console.log(result)
+    console.log('uploaded', file)
+  })
+}
--- ui/index.js
+++ ui/index.js
@@ -1,18 +1,16 @@
 const express = require('express')
 const app = express()
 const cloudinary = require('cloudinary')
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 const filter = 'red_rock' // audrey, ukulele, quartz
 
 app.get('/', function (req, res) {
   cloudinary.v2.api.resources({resource_type: 'image', max_results: 1, direction: 'desc'}, (err, result) => {
-    if(err) return console.log(err)
-    console.log(result.resources)
+    if (err) return console.log(err)
     const urls = result.resources.map((resource) => generateUrl(resource))
 
     res.send(`<img src="${urls[0]}" style="height: 100%;"/>`)
   })
-  console.log(cloudinary)
 })
 
 app.listen(port, function () {
