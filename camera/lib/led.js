e6bc7d3635e31a1cbf187f24c1408b16ae0f5437
Christian Felder (masone) <ema@rh-productions.ch>
2017-12-31 08:58:20 +0100

    disable led

--- camera/lib/led.js
+++ camera/lib/led.js
@@ -10,50 +10,50 @@ const led = new Gpio(15, 'out')
 
 const ready = () => {
   console.log('led ready')
-  on()
+  // on()
 }
 const failure = () => {
   console.log('led failure')
-  const intervals = error()
-  setTimeout(() => {
-    intervals.forEach(interval => clearInterval(interval))
-  }, 1200)
+  // const intervals = error()
+  // setTimeout(() => {
+  //   intervals.forEach(interval => clearInterval(interval))
+  // }, 1200)
 }
 const working = () => {
-  console.log('led workoing')
-  return setInterval(() => {
-    off()
-    setInterval(() => {
-      on()
-    }, 100)
-  }, 100)
+  console.log('led working')
+  // return setInterval(() => {
+  //   off()
+  //   setInterval(() => {
+  //     on()
+  //   }, 100)
+  // }, 100)
 }
 const stopWorking = (working) => {
   console.log('led stop working')
-  clearInterval(working)
+  // clearInterval(working)
 }
 const error = () => {
   console.log('led error')
-  const delay = 1600
-  const intOff = setInterval(() => {
-    off()
-  }, delay)
-  const intOn = setInterval(() => {
-    setTimeout(() => {
-      on()
-    }, delay/2)
-  }, delay)
-
-  return [intOff, intOn]
+  // const delay = 1600
+  // const intOff = setInterval(() => {
+  //   off()
+  // }, delay)
+  // const intOn = setInterval(() => {
+  //   setTimeout(() => {
+  //     on()
+  //   }, delay/2)
+  // }, delay)
+  //
+  // return [intOff, intOn]
 }
 
 const on = () => {
   console.log('led on')
-  led.writeSync(1)
+  // led.writeSync(1)
 }
 const off = () => {
   console.log('led off')
-  led.writeSync(0)
+  // led.writeSync(0)
 }
 
 module.exports = {ready, error, working, stopWorking, failure}
