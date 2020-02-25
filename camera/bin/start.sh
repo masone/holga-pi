f02920e0e378a4edb7d490a379aa93bb18dfbac0
Christian Felder <ema@rh-productions.ch>
2020-02-25 19:44:19 +0000

    move node start

--- camera/bin/start.sh
+++ camera/bin/start.sh
@@ -3,10 +3,9 @@ iwgetid -r
 
 if [ $? -eq 0 ]; then
   printf 'Connected to existing WiFi\n'
+  cd $( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+  sudo -E node ../index.js
 else
   printf 'Starting WiFi Connect\n'
   wifi-connect --portal-ssid=HolgaPi
 fi
-
-cd $( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-sudo -E node ../index.js
