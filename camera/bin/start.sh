eb5236c3628de118c3a1e9e05fd08960172f9d05
Christian Felder <ema@rh-productions.ch>
2020-02-29 09:06:42 +0000

    simplify start command

--- camera/bin/start.sh
+++ camera/bin/start.sh
@@ -3,9 +3,10 @@ iwgetid -r
 
 if [ $? -eq 0 ]; then
   printf 'Connected to existing WiFi\n'
-  cd $( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-  sudo -E node ../index.js
 else
   printf 'Starting WiFi Connect\n'
   wifi-connect --portal-ssid=HolgaPi
 fi
+
+printf 'Starting camera\n'
+node /home/pi/holga-pi/camera/index.js > /home/pi/holga-pi.log
